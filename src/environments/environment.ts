19d863356c1e090acbd8169e5d411d2b4f1e2de2
Gabriel De León <xgabrieldeleon@gmail.com>
2025-03-12 10:44:39 -0300

    feat: implement authentication component with Supabase integration

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://urnfpvprkcyyxfphqeln.supabase.co/',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVybmZwdnBya2N5eXhmcGhxZWxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA2NjI3NTIsImV4cCI6MjA1NjIzODc1Mn0.Ccj7iM7XklYmfhMyp0J4f6HwwPwoWOQtFLozV71TDyU',
+};

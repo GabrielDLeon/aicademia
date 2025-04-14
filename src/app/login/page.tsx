912603f75207343c8a88f0d85d5561393763d5c0
Damián Viera <113361179+Theverimix@users.noreply.github.com>
2025-04-14 10:22:57 -0300

    refactor: Adjust LoginForm padding and update layout in LoginPage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,8 +2,8 @@ import { LoginForm } from "../components/LoginForm";
 
 export default function LoginPage() {
     return (
-        <div className="flex bg-gradient-to-t from-background to-card min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-            <div className="w-full max-w-sm md:max-w-3xl ">
+        <div className="flex bg-gradient-to-t from-background to-card min-h-svh flex-col items-center justify-center bg-muted">
+            <div className="w-full max-w-md">
                 <LoginForm />
             </div>
         </div>
